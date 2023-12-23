d7ca4496c5de23ad26eb8ca675d43ce3e415c2a9
Sakura Kade Hansen <137737608+SakuraKade@users.noreply.github.com>
2023-12-23 13:15:15 +0100

    Fixed main

--- AltF4Enforcer/AltF4Enforcer.cpp
+++ AltF4Enforcer/AltF4Enforcer.cpp
@@ -1,17 +1,11 @@
-#pragma GCC optimize("03")
+#include <Windows.h>
+#include "Application.h"
+#include "ActiveWindowKiller.h"
 
-#include <Windows.h>;
-#include "Application.h";
-#include "ActiveWindowKiller.h";
-
-#ifdef NDEBUG
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     return Application::app_main();
 }
-#endif
 
-#ifdef DEBUG
 int main() {
     return Application::app_main();
 }
-#endif
